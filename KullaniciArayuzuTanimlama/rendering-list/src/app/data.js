be5b98a1086ac90e722f6eae703fcbe96ca5a007
Başar Özenboy <pashaoo@gmail.com>
2024-09-08 22:45:48 +0300

    rendering list next.js ile eklendi

---
+++ KullaniciArayuzuTanimlama/rendering-list/src/app/data.js
@@ -0,0 +1,32 @@
+export const people = [{
+    id: 0, // JSX için anahtar olarak kullanılır
+    name: 'Creola Katherine Johnson',
+    profession: 'mathematician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'MK3eW3A'
+  }, {
+    id: 1, // JSX için anahtar olarak kullanılır
+    name: 'Mario José Molina-Pasquel Henríquez',
+    profession: 'chemist',
+    accomplishment: 'discovery of Arctic ozone hole',
+    imageId: 'mynHUSa'
+  }, {
+    id: 2, // JSX için anahtar olarak kullanılır
+    name: 'Mohammad Abdus Salam',
+    profession: 'physicist',
+    accomplishment: 'electromagnetism theory',
+    imageId: 'bE7W1ji'
+  }, {
+    id: 3, // JSX için anahtar olarak kullanılır
+    name: 'Percy Lavon Julian',
+    profession: 'chemist',
+    accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+    imageId: 'IOjWm71'
+  }, {
+    id: 4, // JSX için anahtar olarak kullanılır
+    name: 'Subrahmanyan Chandrasekhar',
+    profession: 'astrophysicist',
+    accomplishment: 'white dwarf star mass calculations',
+    imageId: 'lrWQx8l'
+  }];
+  
